1ec8d0337d261023a3978770d8394ec91bd9ec10
guanmu <wqguanmu@gmail.com>
2017-05-24 08:55:29 +0800

    日志输出目录支持Linux

--- src/com/guanmu/utils/LogPath.java
+++ src/com/guanmu/utils/LogPath.java
@@ -12,15 +12,42 @@ import ch.qos.logback.core.PropertyDefinerBase;
  * 
  */
 public class LogPath extends PropertyDefinerBase {
-		
+	
+	public static final String OS_NAME = System.getProperty("os.name").toLowerCase();	
+	
+	public static final String WINDOWS_SUFFIX = "C:/";
+	public static final String LINUX_SUFFIX = "/ecc/";	
+	
+	private static final String SUB_LOG_PATH = "ecc/logs";
+	
 	@Override
 	public String getPropertyValue() {
 		
-		return "C:/ecc/logs";
+		if (isWindows()) {
+			return WINDOWS_SUFFIX + SUB_LOG_PATH;			
+		}
 
+		if (isLinux()) {
+			return LINUX_SUFFIX + SUB_LOG_PATH;	
+		}
+		
+		return "ecc/logs";
 	}
 
 
+	
+	/**
+	 * @return
+	 */
+	public static boolean isLinux() {
+		return OS_NAME.indexOf("linux") >= 0;
+	}
 
+	/**
+	 * @return
+	 */
+	public static boolean isWindows() {
+		return OS_NAME.indexOf("windows") >= 0;
+	}
 	
 }
